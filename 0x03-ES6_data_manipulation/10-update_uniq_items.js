56bda9a5969f6b5879cca343ca7d70073b9b57cc
Abrham Bunaro <42895053+Abrish-b@users.noreply.github.com>
2022-06-21 23:31:21 +0300

    quick fix

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@
    accepts a map as an argument */
    export default function updateUniqueItems(map) {
     // if not an instance of map throws an error
-    if (!(map instanceof Map)) throw new Error('Cannot process');
+    if (!(map instance Map)) throw new Error('Cannot process');
     for (const x of map) if (x[1] === 1) map.set(i[0], 100);
     return map;
   }
