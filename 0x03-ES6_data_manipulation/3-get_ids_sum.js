059592e9546f39e73bfbd1df4e3a7fe999ab5c2f
Abrham Bunaro <42895053+Abrish-b@users.noreply.github.com>
2022-06-21 23:35:25 +0300

    fix

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,13 +1,5 @@
 /* Create a function getStudentIdsSum
    Should accept students' list */
-   export default function getStudentIdsSum(students) {
-    // confirm that the argument is an array
-    if (Object.getPrototypeOf(students) === Array.prototype) {
-      // gathering of ids using map
-      const student_ids = students.map((i) => i.student_id);
-      // adding all elements
-      const reducer = (accumulator, currentValue) => accumulator + currentValue;
-      return student_ids.reduce(reducer);
-    }
-    return [];
+   export default function getStudentIdsSum(studentsList) {
+    return studentsList.reduce((prev, { id }) => prev + id, 0);
   }
