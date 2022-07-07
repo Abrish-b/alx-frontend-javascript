554bbd84518ea09b2b1ae8de12ddc7b308ad1a28
Abrham Bunaro <42895053+Abrish-b@users.noreply.github.com>
2022-07-08 00:07:38 +0300

    eslint compliant

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 // create function getListStudentsIds
-export default function getListStudentIds(arr_students) {
-    // confirm that the rg is an array
-    if (!Array.isArray(arr_students)) return [];
-    return arr_students.map((lst) => lst.id);
-  }
+export default function getListStudentIds(arrStudents) {
+  // confirm that the rg is an array
+  if (!Array.isArray(arrStudents)) return [];
+  return arrStudents.map((lst) => lst.id);
+}
