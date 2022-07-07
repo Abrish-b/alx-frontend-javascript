849ad4a57a73d6c4c4367b598a98bf97fdb704f2
Abrham Bunaro <42895053+Abrish-b@users.noreply.github.com>
2022-07-08 00:02:37 +0300

    lint failer fixed

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -5,23 +5,23 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    // getter for sqft object
-    get sqft() {
-        return this._sqft;
-    }
+  // getter for sqft object
+  get sqft() {
+    return this._sqft;
+  }
 
-    // getter for floors object
-    get floors() {
-        return this._floors;
-    }
+  // getter for floors object
+  get floors() {
+    return this._floors;
+  }
 
-    // Overide the method
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this.floors} floors`;
-    }
+  // Overide the method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
