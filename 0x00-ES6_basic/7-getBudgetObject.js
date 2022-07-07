cf84bd54202b9a9343017f4a6db6792cfcd45e87
Abrham Bunaro <42895053+Abrish-b@users.noreply.github.com>
2022-07-07 23:41:08 +0300

    all of styling errors fixed

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,10 @@
 export default function getBudgetObject(income, gdp, capita) {
-    // modify the budget obejct to use the keyname
-    const budget = {
-      income,
-      gdp,
-      capita,
-    };
-  
-    return budget;
-  }
+  // modify the budget obejct to use the keyname
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
